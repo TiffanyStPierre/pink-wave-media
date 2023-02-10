8506a23313d4a035081f95e86e3a496777a2c6a7
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-02-09 17:33:28 -0700

    Completed CSS and Javascript

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.navbar-link');
+const topButton = document.querySelector('.back-to-top');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -14,3 +15,22 @@ navLinks.forEach(link => {
         document.body.classList.toggle('no-scroll');
     })
 });
+
+/*  Scroll to top button functionality  */
+
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}  
